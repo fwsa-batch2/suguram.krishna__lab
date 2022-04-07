687b9202c82ea84493781bf04225ea4bf89f9f61
Suguram Krishna <88901530+SuguramKrishna@users.noreply.github.com>
2022-04-07 12:37:16 +0530

    worked on Java basics

---
+++ Java/Practice_Exercise/Decalring_variable.java
@@ -0,0 +1,14 @@
+public class Decalring_variable {
+    public static void main(String[] args){
+        String name = "Suguram";
+        int roll_no = 21;
+        float temp = 32.6f;
+        char attendance = 'p';
+        Boolean status = true;
+        System.out.println(name);
+        System.out.println(roll_no);
+        System.out.println(attendance);
+        System.out.println(status);
+        System.out.println(temp);
+    }
+}
---
+++ Java/Practice_Exercise/Multiple_variables.java
@@ -0,0 +1,6 @@
+public class Multiple_variables{
+    public static void main (String[] args){
+        int a = 10, b = 21, c = 30;
+        System.out.println(a+b+c);
+    }
+}
