157060c89a9d7fefd4082f8a8a2af0b43b6f2245
Suguram Krishna <88901530+SuguramKrishna@users.noreply.github.com>
2022-05-03 15:20:56 +0530

    Created a calc with Inheritance

---
+++ Java/Programs/com/ENC/com/ENC/calcUsingInheritance.java
@@ -0,0 +1,27 @@
+package com.ENC;
+
+class Calc {
+    public int add(int firstNum,int secondNum){
+        return firstNum+secondNum;
+    }
+    public int sub(int firstNum,int secondNum){
+        return firstNum-secondNum;
+    }
+}
+
+class SuperCalc extends Calc{
+    public int modulo(int firstNum, int secondNum){
+        return firstNum%secondNum; 
+    }
+}     
+public class calcUsingInheritance {
+    public static void main(String[] args) {
+    SuperCalc sumClass = new SuperCalc();
+    int sum = sumClass.add(12,14);
+    int sub = sumClass.sub(12,4);
+    int modulo = sumClass.modulo(12,10);
+    System.out.println(sum);
+    System.out.println(sub);
+    System.out.println(modulo);
+    }
+}
---
+++ Java/Programs/com/ENC/com/ENC/defaultSpecifier.java
@@ -0,0 +1,18 @@
+package com.ENC;
+
+
+class defaultSpecifier {
+    private int defaultCheck;
+    private String passwordCheck;
+    
+    public defaultSpecifier(){
+        defaultCheck = 12;
+        passwordCheck = "Sk123@gmail.com";
+    }
+
+    public static void main(String[] args) {
+        defaultSpecifier check = new defaultSpecifier();
+        System.out.println(check.defaultCheck);
+        System.out.println(check.passwordCheck);
+    }
+}
