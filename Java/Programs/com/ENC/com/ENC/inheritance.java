f9b2ed1469444826aee3bc5dd6ed3e1e0fef1042
Suguram Krishna <88901530+SuguramKrishna@users.noreply.github.com>
2022-05-06 09:18:04 +0530

    Pushed the Unpushed Exercises

--- Java/Programs/com/ENC/com/ENC/inheritance.java
+++ Java/Programs/com/ENC/com/ENC/inheritance.java
@@ -2,14 +2,16 @@ package com.ENC;
 
 class inheritance {
     public void firstOut(){
-        System.out.println("Hello!! This is First one");
+        // System.out.println("Hello!! This is First one");
+        System.out.println("Thank you guys Varataa!!!!");
     }
 }
 
 class inher1   extends inheritance{
     public  void secondOut()
     {
-        System.out.println("This is Second one");
+        // System.out.println("This is Second one");
+        System.out.println("If you want to contact mail to www.pichu.com");
     }
 
     public static void main(String[] args) {
