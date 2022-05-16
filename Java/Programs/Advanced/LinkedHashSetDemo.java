bc3f9cda51bed58658d7cf48c3cebb12099c56c6
Suguram Krishna <88901530+SuguramKrishna@users.noreply.github.com>
2022-05-16 23:54:18 +0530

    added some for files

---
+++ Java/Programs/Advanced/LinkedHashSetDemo.java
@@ -0,0 +1,13 @@
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+    public static void main(String[] args) {
+    LinkedHashSet<Integer> newhs = new LinkedHashSet<Integer>();
+        newhs.add(14);
+        newhs.add(27);
+        newhs.add(45);
+        newhs.add(18);
+        newhs.add(21);
+        System.out.println(newhs);
+    }
+}
---
+++ Java/Programs/Advanced/SetDemo.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        HashSet<Integer> newhs = new HashSet<Integer>();
+        newhs.add(14);
+        newhs.add(27);
+        newhs.add(45);
+        newhs.add(18);
+        newhs.add(21);
+        System.out.println(newhs);
+    }
+}
