dc9af9a9f14758b98928c47a9a1e6a360a33753d
Suguram Krishna <88901530+SuguramKrishna@users.noreply.github.com>
2022-10-20 23:34:27 +0530

    backup push

--- Todo/db/migrate/20220624115148_renamingcolumns.rb
+++
@@ -1,8 +0,0 @@
-class Renamingcolumns < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :todo_models , :Description , :description
-    rename_column :todo_models , :Task , :task
-    rename_column :todo_models , :Status , :status
-    rename_column :todo_models , :Date , :date
-  end
-end
--- Todo/db/migrate/20220627105954_adding_default_to_status.rb
+++
@@ -1,5 +0,0 @@
-class AddingDefaultToStatus < ActiveRecord::Migration[6.1]
-  def change
-    change_column :todo_models, :status, :boolean, default: false
-  end
-end
